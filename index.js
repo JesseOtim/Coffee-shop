38334b7f5afe211299663842c1fa76e9606d4f4f
Jesse Otim <otimdanieljesse@gmail.com>
2023-09-24 10:17:39 +0300

    Refactor code

--- index.js
+++ index.js
@@ -90,77 +90,147 @@ const Validate = (event) => {
 }
 
 
-  // // Get references to the input field and search button
-  // const searchInput = document.getElementById('searchInput');
-  // const searchButton = document.getElementById('searchButton');
+//   // // Get references to the input field and search button
+//   // const searchInput = document.getElementById('searchInput');
+//   // const searchButton = document.getElementById('searchButton');
 
-  // // Add a click event listener to the search button
-  // searchButton.addEventListener('click', function() {
-  //     // Get the value of the input field
-  //     const searchTerm = searchInput.value.trim(); // trim removes leading/trailing whitespace
+//   // // Add a click event listener to the search button
+//   // searchButton.addEventListener('click', function() {
+//   //     // Get the value of the input field
+//   //     const searchTerm = searchInput.value.trim(); // trim removes leading/trailing whitespace
 
-  //     // Check if the input is empty
-  //     if (searchTerm === '') {
-  //         alert('Please enter a search term.'); // You can replace this with your preferred validation method
-  //     } else {
-  //         // Perform the search or other action here
-  //         alert(`Searching for: ${searchTerm}`);
-  //         // Replace the alert with the actual search logic
-  //     }
-  // });
+//   //     // Check if the input is empty
+//   //     if (searchTerm === '') {
+//   //         alert('Please enter a search term.'); // You can replace this with your preferred validation method
+//   //     } else {
+//   //         // Perform the search or other action here
+//   //         alert(`Searching for: ${searchTerm}`);
+//   //         // Replace the alert with the actual search logic
+//   //     }
+//   // });
 
-//   document.getElementById('liveAlertBtn').addEventListener('click', function () {
-//     const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
-//     const alertHTML = `
-//     <div class="alert alert-success alert-dismissible fade show" role="alert">
-//         This is a live Bootstrap alert! You can customize its content.
-//         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-//             <span aria-hidden="true">&times;</span>
-//         </button>
-//     </div>
-//     `;
+// //   document.getElementById('liveAlertBtn').addEventListener('click', function () {
+// //     const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+// //     const alertHTML = `
+// //     <div class="alert alert-success alert-dismissible fade show" role="alert">
+// //         This is a live Bootstrap alert! You can customize its content.
+// //         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+// //             <span aria-hidden="true">&times;</span>
+// //         </button>
+// //     </div>
+// //     `;
 
-//     // Clear any existing alerts
-//     alertPlaceholder.innerHTML = '';
+// //     // Clear any existing alerts
+// //     alertPlaceholder.innerHTML = '';
 
-//     // Insert the new alert
-//     alertPlaceholder.insertAdjacentHTML('beforeend', alertHTML);
+// //     // Insert the new alert
+// //     alertPlaceholder.insertAdjacentHTML('beforeend', alertHTML);
 
-//     // You can also use Bootstrap's JavaScript functions to initialize the alert (optional)
-//     // For example, to close the alert after a few seconds:
-//     const alertElement = document.querySelector('.alert');
-//     if (alertElement) {
-//         const bsAlert = new bootstrap.Alert(alertElement);
-//         setTimeout(function () {
-//             bsAlert.close();
-//         }, 5000); // Close the alert after 5 seconds
-//     }
+// //     // You can also use Bootstrap's JavaScript functions to initialize the alert (optional)
+// //     // For example, to close the alert after a few seconds:
+// //     const alertElement = document.querySelector('.alert');
+// //     if (alertElement) {
+// //         const bsAlert = new bootstrap.Alert(alertElement);
+// //         setTimeout(function () {
+// //             bsAlert.close();
+// //         }, 5000); // Close the alert after 5 seconds
+// //     }
+// // });
+
+// // document.getElementById('liveAlertBtn').addEventListener('click', function () {
+// //   const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+// //   const alertHTML = `
+// //   <div class="alert alert-success alert-dismissible fade show" role="alert">
+// //       This is a live Bootstrap alert! You can customize its content.
+// //       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+// //           <span aria-hidden="true">&times;</span>
+// //       </button>
+// //   </div>
+// //   `;
+
+// //   // Clear any existing alerts
+// //   alertPlaceholder.innerHTML = '';
+
+// //   // Insert the new alert
+// //   alertPlaceholder.insertAdjacentHTML('beforeend', alertHTML);
+
+// //   // You can also use Bootstrap's JavaScript functions to initialize the alert (optional)
+// //   // For example, to close the alert after a few seconds:
+// //   const alertElement = document.querySelector('.alert');
+// //   if (alertElement) {
+// //       const bsAlert = new bootstrap.Alert(alertElement);
+// //       setTimeout(function () {
+// //           bsAlert.close();
+// //       }, 5000); // Close the alert after 5 seconds
+// //   }
+// // });
+// const ValidatePassword = (password) => {
+//   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+//   if (password === "") {
+//       return {
+//           isValid: false,
+//           errorMessage: "Password can't be blank",
+//       };
+//   } else if (!password.match(passwordRegex)) {
+//       return {
+//           isValid: false,
+//           errorMessage: "Please put in the correct password",
+//       };
+//   } else {
+//       return {
+//           isValid: true,
+//       };
+//   }
+// };
+
+// const ValidateEmail = (email) => {
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//   if (email === "") {
+//       return {
+//           isValid: false,
+//           errorMessage: "Email can't be blank",
+//       };
+//   } else if (!email.match(emailRegex)) {
+//       return {
+//           isValid: false,
+//           errorMessage: "Please enter a valid email address",
+//       };
+//   } else {
+//       return {
+//           isValid: true,
+//       };
+//   }
+// };
+
+// const passwordInput = document.getElementById("password");
+// const emailInput = document.getElementById("email");
+// const passwordError = document.getElementById("passwordErr");
+// const emailError = document.getElementById("emailErr");
+
+// passwordInput.addEventListener("input", () => {
+//   const password = passwordInput.value.trim();
+//   const validationResult = ValidatePassword(password);
+
+//   if (validationResult.isValid) {
+//       passwordInput.style.border = "2px solid green";
+//       passwordError.innerHTML = "";
+//   } else {
+//       passwordInput.style.border = "";
+//       passwordError.innerHTML = validationResult.errorMessage;
+//   }
 // });
 
-// document.getElementById('liveAlertBtn').addEventListener('click', function () {
-//   const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
-//   const alertHTML = `
-//   <div class="alert alert-success alert-dismissible fade show" role="alert">
-//       This is a live Bootstrap alert! You can customize its content.
-//       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-//           <span aria-hidden="true">&times;</span>
-//       </button>
-//   </div>
-//   `;
+// emailInput.addEventListener("input", () => {
+//   const email = emailInput.value.trim();
+//   const validationResult = ValidateEmail(email);
 
-//   // Clear any existing alerts
-//   alertPlaceholder.innerHTML = '';
-
-//   // Insert the new alert
-//   alertPlaceholder.insertAdjacentHTML('beforeend', alertHTML);
-
-//   // You can also use Bootstrap's JavaScript functions to initialize the alert (optional)
-//   // For example, to close the alert after a few seconds:
-//   const alertElement = document.querySelector('.alert');
-//   if (alertElement) {
-//       const bsAlert = new bootstrap.Alert(alertElement);
-//       setTimeout(function () {
-//           bsAlert.close();
-//       }, 5000); // Close the alert after 5 seconds
+//   if (validationResult.isValid) {
+//       emailInput.style.border = "2px solid green";
+//       emailError.innerHTML = "";
+//   } else {
+//       emailInput.style.border = "";
+//       emailError.innerHTML = validationResult.errorMessage;
 //   }
 // });
